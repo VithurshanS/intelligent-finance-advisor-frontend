29e3de1fc60c001bcc89090d1fcc1611df4a8d1f
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-04-15 00:57:07 +0530

    Add theme provider and mode toggle component; update layout and styles Changed sidebar primary foreground color

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,35 +1,41 @@
-import { AppSidebar } from "@/app/(dashboard)/_components/app-sidebar"
-import { Separator } from "@/components/ui/separator"
+import {AppSidebar} from "@/app/(dashboard)/_components/app-sidebar"
+import {Separator} from "@/components/ui/separator"
 import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { Bell, LogOut } from "lucide-react"
+import {Bell, LogOut} from "lucide-react"
+import {ModeToggle} from "@/components/ThemeProvider";
 
-export default function DashboardLayout({children}: {children: React.ReactNode}) {
+export default function DashboardLayout({children}: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
-            <AppSidebar />
+            <AppSidebar/>
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center justify-between transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <header
+                    className="flex h-16 shrink-0 items-center justify-between transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
+                        <SidebarTrigger className="-ml-1"/>
                         <Separator
                             orientation="vertical"
                             className="mr-2 data-[orientation=vertical]:h-4"
                         />
                     </div>
                     <div className="flex items-center gap-4 px-4">
+                        <ModeToggle/>
+                        <Separator
+                            orientation="vertical"
+                            className="data-[orientation=vertical]:h-4"/>
                         <button className="p-2 rounded-lg hover:bg-gray-100">
-                            <Bell size={16} />
+                            <Bell size={16}/>
                         </button>
                         <Separator
                             orientation="vertical"
                             className="data-[orientation=vertical]:h-4"
                         />
                         <button className="p-2 rounded-lg hover:bg-gray-100">
-                            <LogOut size={16} />
+                            <LogOut size={16}/>
                         </button>
                     </div>
                 </header>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import {ToastContainer} from "react-toastify";
 import {GeistSans} from 'geist/font/sans';
 import {GeistMono} from 'geist/font/mono';
+import {ThemeProvider} from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
     title: "Intelligent Personal Finance Assistant",
@@ -21,7 +22,14 @@ export default function RootLayout({
         <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
         <body className={'bg-background h-screen w-screen flex'}>
         <div className={'w-full h-full'}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
             {children}
+            </ThemeProvider>
         </div>
         <ToastContainer/>
         </body>
