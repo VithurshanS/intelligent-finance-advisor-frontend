56f6e843a68d539c31cddea283a8a9c5e5c2e4fa
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-04-14 23:57:57 +0530

    Replace placeholder image with Next.js Image component for the login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,13 @@
 
 import { Wallet } from "lucide-react"
 import { LoginForm } from "@/components/login-form"
+import Image from "next/image";
 
 export default function LoginPage() {
     return (
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col gap-4 p-6 md:p-10">
-                <div className="flex justify-center gap-2 md:justify-start">
+                <div className="flex justify-start gap-2">
                     <a href="#" className="flex items-center gap-2 font-medium">
                         <div className="bg-sidebar-primary text-sidebar-primary-foreground flex size-8 items-center justify-center rounded-lg">
                             <Wallet className="size-4" />
@@ -25,11 +26,7 @@ export default function LoginPage() {
                 </div>
             </div>
             <div className="bg-muted relative hidden lg:block">
-                <img
-                    src="/placeholder.svg"
-                    alt="Image"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
+                <Image src={'/login-image.jpg'} alt={'Login Image'} fill className="object-cover" />
             </div>
         </div>
     )
