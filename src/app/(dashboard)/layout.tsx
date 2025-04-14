0a65fe575cd919c3b64e64f5b27af30e0ef3dbb8
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-04-15 01:21:44 +0530

    removed a separator

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,10 +18,6 @@ export default function DashboardLayout({children}: { children: React.ReactNode 
                     className="flex h-16 shrink-0 items-center justify-between transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1"/>
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
                     </div>
                     <div className="flex items-center gap-4 px-4">
                         <ModeToggle/>
