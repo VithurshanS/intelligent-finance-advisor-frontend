37e893f16a80bbb44d71686db69a8cd94b005fbe
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-04-30 19:46:45 +0530

    Remove unused links

--- src/app/(dashboard)/_components/app-sidebar-links.tsx
+++ src/app/(dashboard)/_components/app-sidebar-links.tsx
@@ -6,92 +6,20 @@ import {NavGuides} from "@/app/(dashboard)/_components/nav-guides";
 import {Role} from "@/lib/types/user";
 import {SidebarData} from "@/app/(dashboard)/_utils/types";
 import {
-    Bot,
     ChartCandlestickIcon,
-    DollarSign, Earth,
+    Earth,
     FolderGit2,
     LayoutGrid,
     PieChart,
-    Scale,
-    Settings2,
     Users
 } from "lucide-react";
 
 const userSidebar: SidebarData = {
     navMain: [
         {
-            title: "Dashboard",
-            url: "/dashboard",
-            icon: LayoutGrid,
-        },
-        {
-            title: "Market Prediction",
-            url: "/group-37",
-            icon: Bot,
-            items: [
-                {
-                    title: "Price Forecast",
-                    url: "/group-37/forecast",
-                },
-                {
-                    title: "Model Metrics",
-                    url: "/group-37/metrics",
-                },
-            ],
-        },
-        {
-            title: "Budget Tracking",
-            url: "/group-38",
-            icon: DollarSign,
-            items: [
-                {
-                    title: "Categorization",
-                    url: "/group-38/categorization",
-                },
-                {
-                    title: "Budget Analysis",
-                    url: "/group-38/analysis",
-                },
-            ],
-        },
-        {
             title: "Portfolio Optimization",
             url: "/dashboard/portfolio",
             icon: PieChart,
-        },
-        {
-            title: "Risk & Compliance",
-            url: "/group-40",
-            icon: Scale,
-            items: [
-                {
-                    title: "Risk Analysis",
-                    url: "/group-40/risk",
-                },
-                {
-                    title: "Fairness & Bias",
-                    url: "/group-40/fairness",
-                },
-                {
-                    title: "Explainability",
-                    url: "/group-40/explain",
-                },
-            ],
-        },
-        {
-            title: "Settings",
-            url: "/settings",
-            icon: Settings2,
-            items: [
-                {
-                    title: "General",
-                    url: "/settings/general",
-                },
-                {
-                    title: "Team",
-                    url: "/settings/team",
-                },
-            ],
         },
     ],
     guides: [
