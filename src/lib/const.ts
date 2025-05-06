7d015771af322147b517a81aa0ddfe798ffc7c8d
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-05-06 22:17:28 +0530

    Update BACKEND_BASE_URL to point to production server

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,1 @@
-// Make sure to point to the correct backend URL for client-side requests
-
-export const BACKEND_BASE_URL = "http://localhost:8000";
+export const BACKEND_BASE_URL = "https://financial-advisor.shancloudservice.com";
