9fb3f0105368b3b19653db9fa2a3ddd3d054c829
Vithurshan Sivanathan <155006746+VithurshanS@users.noreply.github.com>
2025-05-18 15:58:09 +0530

    Update actions.ts

--- src/app/(dashboard)/global-assets/[type]/_utils/actions.ts
+++ src/app/(dashboard)/global-assets/[type]/_utils/actions.ts
@@ -116,8 +116,7 @@ export async function createStockAction(ticker: string): Promise<CreateStockResp
         const response = await AxiosInstance.post('/assets/create-stock', null, {
             params: {ticker},
         });
-        const res = await AxiosInstance.post('/train_model',{"ticker_symbol":ticker})
-        console.log(res.status)
+
 
         revalidatePath('/admin/stocks');
         revalidatePath('/assets');
